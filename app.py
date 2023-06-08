001ad1884136b9b843a7dba7809df584fc8dfcd7
try n CATch <zulnurdiana@gmail.com>
2023-06-08 10:24:19 +0700

    fix minor on predict endpoint (solved)

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import tensorflow as tf
 import numpy as np
 import joblib
 
+
 app = Flask(__name__)
 
 model_greenify = "deliverable/model.h5"
-model_testing = "deliverable/modeltest.h5"
-
+scalerTourism = joblib.load('deliverable/scalerTourism.save')
+scalerUser = joblib.load('deliverable/scalerUser.save')
+scalerTarget = joblib.load('deliverable/scalerTarget.save')
 
 try:
     # Membaca model dari file .h5
@@ -16,38 +18,35 @@ try:
 except (OSError, IOError) as e:
     print("Gagal memuat model:", str(e))
 
-# scalerTourism = joblib.load('deliverable/scalerTourism.save')
-# scalerUser = joblib.load('deliverable/scalerUser.save')
-# scalerTarget = joblib.load('deliverable/scalerTarget.save')
-
 
 @app.route('/prediksi', methods=['POST'])
 def prediksi_rating():
+
     # Mendapatkan data JSON dari permintaan
     data = request.json
 
     # Mengekstrak user_feature dari data permintaan
     user_feature = np.array(data['user_feature'])
+    user_feature = scalerUser.transform(user_feature)
 
     # Mengekstrak tempat_feature dari data permintaan
     tempat_feature = np.array(data['tempat_feature'])
+    tempat_feature = scalerTourism.transform(tempat_feature)
 
     # Menduplikasi user_feature untuk setiap tempat
     user_feature = np.repeat(user_feature, tempat_feature.shape[0], axis=0)
 
     # Menggabungkan user_feature dan tempat_feature
-    features = np.concatenate((np.expand_dims(
-        user_feature, axis=0), np.expand_dims(tempat_feature, axis=0)), axis=0)
+    features = [user_feature, tempat_feature]
 
     # Melakukan prediksi menggunakan model yang dimuat
     ratings = model.predict(features)
+    ratings = scalerTarget.inverse_transform(ratings)
 
-    # Mengurutkan rating secara menurun dan mendapatkan indeksnya
-    sorted_indices = np.argsort(ratings)[::-1]
+    # Sorting berdasarkan index
+    sorted_index = np.argsort(-ratings, axis=0).reshape(-1).tolist()
 
-    # Mengembalikan indeks yang sudah diurutkan sebagai hasil prediksi
-    predictions = sorted_indices.tolist()
-    return jsonify(predictions)
+    return jsonify(sorted_index)
 
 
 if __name__ == '__main__':
